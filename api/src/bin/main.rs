e418ad8dc89f28631c67b024c1d240606e2f0b97
watanaberyunosuke <harrybwatson@protonmail.com>
2025-09-01 23:26:00 +1000

    Apply formater

--- api/src/bin/main.rs
+++ api/src/bin/main.rs
@@ -4,11 +4,14 @@ use api::post_handler;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/api", routes![
-        post_handler::list_posts_handler,
-        post_handler::list_post_handler,
-        post_handler::create_post_handler,
-        post_handler::publish_post_handler,
-        post_handler::delete_post_handler,
-    ])
+    rocket::build().mount(
+        "/api",
+        routes![
+            post_handler::list_posts_handler,
+            post_handler::list_post_handler,
+            post_handler::create_post_handler,
+            post_handler::publish_post_handler,
+            post_handler::delete_post_handler,
+        ],
+    )
 }
